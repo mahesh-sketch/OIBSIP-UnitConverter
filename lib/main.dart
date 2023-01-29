a92d7c171fd1a48cc99e8a7dfd59ec43787916a0
mahesh dash <maheshdash84821@gmail.com>
2023-01-29 13:50:12 +0530

    Unitconverter app

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/',
+    routes: {
+      '/': (context) => home(),
+    },
+  ));
+}
